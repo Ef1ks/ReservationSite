174e6007158fb51d87e0b99585331c977eaf7a2b
Kacper Prusik <kacper@betadev.pl>
2026-03-07 15:20:34 +0100

    startup project

---
+++ src/main/java/com/cinefile/reservationsite/ReservationSiteApplication.java
@@ -0,0 +1,13 @@
+package com.cinefile.reservationsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cinefile/reservationsite/ReservationSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinefile.reservationsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
